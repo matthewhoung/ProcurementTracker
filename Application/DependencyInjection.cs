670b0ccb1e0a9a61bc963d3abd563dbddfc1384c
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-06-04 18:15:15 +0800

    加入專案檔案。

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using FluentValidation;
+
+namespace Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            var assembly = typeof(DependencyInjection).Assembly;
+
+            services.AddMediatR(config =>
+                config.RegisterServicesFromAssemblies(assembly));
+
+            services.AddValidatorsFromAssembly(assembly);
+
+            return services;
+        }
+    }
+}
