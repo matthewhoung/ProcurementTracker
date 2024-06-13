367c09ae30dc112cfcd8b82343dd15c997125f47
DESKTOP-FTVMTJF\user <matthewhoung@gmail.com>
2024-06-13 11:17:06 +0800

    added read data access logic

--- Domain/Interfaces/IGenericRepository.cs
+++ Domain/Interfaces/IGenericRepository.cs
@@ -4,6 +4,7 @@ namespace Domain.Interfaces
 {
     public interface IGenericRepository
     {
+        // Create section
         Task CreateWorkerClassAsync(WorkerClass workerClass);
         Task CreateWorkerTypeAsync(WorkerType workerType);
         Task CreateWorkerTeamAsync(WorkerTeam workerTeam);
@@ -12,5 +13,14 @@ namespace Domain.Interfaces
         Task CreatePayTypeAsync(PayType payType);
         Task CreateUnitAsync(UnitClass unitClass);
         Task CreateRolesAsync(Roles roles);
+        // Read section
+        Task<List<WorkerClass>> GetAllWorkerClassesAsync();
+        Task<List<WorkerType>> GetAllWorkerTypesAsync();
+        Task<List<WorkerTeam>> GetAllWorkerTeamsAsync();
+        Task<List<Department>> GetAllDepartmentsAsync();
+        Task<List<PayBy>> GetAllPayBysAsync();
+        Task<List<PayType>> GetAllPayTypesAsync();
+        Task<List<UnitClass>> GetAllUnitsAsync();
+        Task<List<Roles>> GetAllRolesAsync();
     }
 }
--- Infrastructure/Repositories/GenericRepository.cs
+++ Infrastructure/Repositories/GenericRepository.cs
@@ -13,6 +13,7 @@ namespace Infrastructure.Repositories
         {
             _dbConnection = dbConnection;
         }
+        // Create section
         public async Task CreateDepartmentAsync(Department department)
         {
             var writeCommand = @"
@@ -27,7 +28,6 @@ namespace Infrastructure.Repositories
             };
             await _dbConnection.ExecuteAsync(writeCommand, parameters);
         }
-
         public async Task CreatePayByAsync(PayBy payBy)
         {
             var writeCommand = @"
@@ -42,7 +42,6 @@ namespace Infrastructure.Repositories
             };
             await _dbConnection.ExecuteAsync(writeCommand, parameters);
         }
-
         public async Task CreatePayTypeAsync(PayType payType)
         {
             var writeCommand = @"
@@ -57,7 +56,6 @@ namespace Infrastructure.Repositories
             };
             await _dbConnection.ExecuteAsync(writeCommand, parameters);
         }
-
         public async Task CreateRolesAsync(Roles roles)
         {
             var writeCommand = @"
@@ -71,7 +69,6 @@ namespace Infrastructure.Repositories
             };
             await _dbConnection.ExecuteAsync(writeCommand, parameters);
         }
-
         public async Task CreateUnitAsync(UnitClass unitClass)
         {
             var writeCommand = @"
@@ -86,7 +83,6 @@ namespace Infrastructure.Repositories
             };
             await _dbConnection.ExecuteAsync(writeCommand, parameters);
         }
-
         public async Task CreateWorkerClassAsync(WorkerClass workerClass)
         {
             var writeCommand = @"
@@ -101,7 +97,6 @@ namespace Infrastructure.Repositories
             };
             await _dbConnection.ExecuteAsync(writeCommand, parameters);
         }
-
         public async Task CreateWorkerTeamAsync(WorkerTeam workerTeam)
         {
             var writeCommand = @"
@@ -139,7 +134,6 @@ namespace Infrastructure.Repositories
             };
             await _dbConnection.ExecuteAsync(writeCommand, parameters);
         }
-
         public async Task CreateWorkerTypeAsync(WorkerType workerType)
         {
             var writeCommand = @"
@@ -154,6 +148,110 @@ namespace Infrastructure.Repositories
                 WorkerTypeName = workerType.WorkerTypeName 
             };
             await _dbConnection.ExecuteAsync(writeCommand, parameters);
+        }
+        // Read section
+
+        public async Task<List<WorkerClass>> GetAllWorkerClassesAsync()
+        {
+            var readCommand = @"
+                SELECT 
+                    worker_class_id AS WorkerClassId,
+                    worker_class_name AS WorkerClassName
+                FROM 
+                    worker_classes";
+            var workerClasses = await _dbConnection.QueryAsync<WorkerClass>(readCommand);
+            return workerClasses.AsList();
+        }
+
+        public async Task<List<WorkerType>> GetAllWorkerTypesAsync()
+        {
+            var readCommand = @"
+                SELECT 
+                    worker_type_id AS WorkerTypeId,
+                    worker_class_id AS WorkerClassId,
+                    worker_type_name AS WorkerTypeName
+                FROM 
+                    worker_types";
+            var workerTypes = await _dbConnection.QueryAsync<WorkerType>(readCommand);
+            return workerTypes.AsList();
+        }
+
+        public async Task<List<WorkerTeam>> GetAllWorkerTeamsAsync()
+        {
+            var readCommand = @"
+                SELECT 
+                    worker_team_id AS WorkerTeamId,
+                    worker_type_id AS WorkerTypeId,
+                    worker_team_name AS WorkerTeamName,
+                    worker_contact_name AS ContactName,
+                    worker_contact_number AS ContactNumber,
+                    worker_contact_address AS ContactAddress,
+                    worker_account_code AS AccountCode,
+                    worker_account_name AS AccountName,
+                    worker_account_number AS AccountNumber
+                FROM 
+                    worker_teams";
+            var workerTeams = await _dbConnection.QueryAsync<WorkerTeam>(readCommand);
+            return workerTeams.AsList();
+        }
+
+        public async Task<List<Department>> GetAllDepartmentsAsync()
+        {
+            var readCommand = @"
+                SELECT 
+                    department_id AS DepartmentId,
+                    department_name AS DepartmentName
+                FROM 
+                    departments";
+            var departments = await _dbConnection.QueryAsync<Department>(readCommand);
+            return departments.AsList();
+        }
+
+        public async Task<List<PayBy>> GetAllPayBysAsync()
+        {
+            var readCommand = @"
+                SELECT 
+                    pay_by_id AS PayById,
+                    pay_by_name AS PayByName
+                FROM
+                    pay_by";
+            var payBys = await _dbConnection.QueryAsync<PayBy>(readCommand);
+            return payBys.AsList();
+        }
+
+        public async Task<List<PayType>> GetAllPayTypesAsync()
+        {
+            var readCommand = @"
+                SELECT 
+                    pay_type_id AS PayTypeId,
+                    pay_type_name AS PayTypeName
+                FROM
+                    pay_types";
+            var payTypes = await _dbConnection.QueryAsync<PayType>(readCommand);
+            return payTypes.AsList();
+        }
+
+        public async Task<List<Roles>> GetAllRolesAsync()
+        {
+            var readCommand = @"
+                SELECT 
+                    role_name AS RoleName
+                FROM
+                    roles";
+            var roles = await _dbConnection.QueryAsync<Roles>(readCommand);
+            return roles.AsList();
+        }
+
+        public async Task<List<UnitClass>> GetAllUnitsAsync()
+        {
+            var readCommand = @"
+                SELECT 
+                    unit_id AS UnitId,
+                    unit_name AS UnitName
+                FROM
+                    units";
+            var units = await _dbConnection.QueryAsync<UnitClass>(readCommand);
+            return units.AsList();
         }
     }
 }
